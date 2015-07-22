d6b7b55c6d4c97790243f632e2fecc0f3c93434a
Pixize <capelle.florentin@gmail.com>
2015-07-23 00:01:53 +0200

    all unit in dictionnary ressource

--- WarArmy/WarArmy/MainWindow.xaml.cs
+++ WarArmy/WarArmy/MainWindow.xaml.cs
@@ -26,5 +26,54 @@ namespace WarArmy
             InitializeComponent();
             
         }
+
+        private void Button_Annuler_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult de = MessageBox.Show("Vous allez vider l'ensemble des champs rempli précédement ?", "supprimer", MessageBoxButton.OKCancel);
+
+            if (de == MessageBoxResult.OK)
+            {
+                cleanField();
+        
+            }
+        }
+
+
+        public void cleanField()
+        {
+            // clear of Our army
+            OA_Arme.Clear();
+            OA_Cdg.Clear();
+            OA_Chev.Clear();
+            OA_Comm.Clear();
+            OA_def.Clear();
+            OA_G.Clear();
+            OA_Gs.Clear();
+            OA_Merc.Clear();
+            OA_Mut.Clear();
+            OA_Nucl.Clear();
+            OA_Pal.Clear();
+            OA_Pil.Clear();
+            OA_Prot.Clear();
+            OA_SupMut.Clear();
+
+            //cleat of ennemy army
+            EA_Arme.Clear();
+            EA_Cdg.Clear();
+            EA_Chev.Clear();
+            EA_Comm.Clear();
+            EA_Def.Clear();
+            EA_G.Clear();
+            EA_Gs.Clear();
+            EA_Merc.Clear();
+            EA_Mut.Clear();
+            EA_Nucl.Clear();
+            EA_Pal.Clear();
+            EA_Pil.Clear();
+            EA_Prot.Clear();
+            EA_SupMut.Clear();
+        }
+            
+        
     }
 }
